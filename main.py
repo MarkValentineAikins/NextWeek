87dcedda3e027f104d6e6e74a698c10be80c42bc
Mark Valentine Aikins <valentineaikins@icloud.com>
2022-10-18 14:58:24 +0000

    Variables, String method and casting type

---
+++ main.py
@@ -0,0 +1,60 @@
+#String variable assignment
+#Test = "NextWeek"
+#print(Test)
+#Checking datatype assignment
+#print(type(Test))
+#Integer variable assignment
+#Scores = 120
+#print(Scores)
+#print(type(Scores))
+#concatenated variable assignment of different types
+#print("Your scores are: " + str(Scores) + "cm")
+#Float variable assignment
+#Width = 100.05
+#print(type(Width))
+#print("The width of your scores is: " + str(Width) + "cm")
+#Boolean variable assignment
+#is_Human =  True
+#print(type(is_Human))
+#print("The is_human flag is: " + str(is_Human))
+
+#Multiple variables assignment
+#Test = "Scores"
+#Width = 100.05
+#value = 12
+#is_Human = True
+#Test, Width, value, is_Human = "Scores", 100.05, 12, True
+#print(Width)
+#print(value)
+#print(is_Human)
+#print(type(Test))
+#print(type(Width))
+#print(type(value))
+#print(type(is_Human))
+#Using String methods
+fante_Boy = "mark valentine aikins"
+print(fante_Boy)
+print(len(fante_Boy))
+print(fante_Boy.find("t"))
+print(fante_Boy.capitalize())
+print(fante_Boy.upper())
+print(fante_Boy.lower())
+print(fante_Boy.isdigit())
+print(fante_Boy.isalpha())
+print(fante_Boy.isnumeric())
+print(fante_Boy.count("a"))
+print(fante_Boy.replace("k", "c"))
+x = 12
+y = 1.23
+z = "10"
+print(x, y, z)
+x = float(x)
+z = int(z)
+y = str(y)
+
+print ("The new value of Z is " + str(z))
+print ("The new value of Y is " + str(y))
+print ("The new value of X is " + str(x))
+
+
+
